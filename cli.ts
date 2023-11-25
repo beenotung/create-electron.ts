633fa261147b44283851b244715b01b21b910a53
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-11-25 22:03:50 +0800

    chore: inline template.ts into cli.ts

--- cli.ts
+++ cli.ts
@@ -1,12 +1,17 @@
 import { mkdirSync, writeFileSync } from 'fs'
-import { templates } from './template'
 import { getDest } from 'npm-init-helper'
 import { basename, join } from 'path'
 
 async function main() {
   let dest = await getDest()
   mkdirSync(dest)
-  for (let template of templates) {
+
+  type Template = {
+    filename: string
+    content: string | object
+  }
+
+  function setupTemplate(template: Template) {
     let file = join(dest, template.filename)
     let content = template.content
 
@@ -14,17 +19,133 @@ async function main() {
       content = JSON.stringify(content, null, 2)
     }
 
-    if (template.filename == 'index.html') {
-      let title = basename(dest)
-      content = content.replace(
-        '<title>Electron App</title>',
-        `<title>${title}</title>`,
-      )
-    }
-
     writeFileSync(file, content)
     console.log('created:', template.filename)
   }
+
+  let title = basename(dest)
+
+  setupTemplate({
+    filename: 'index.html',
+    content: /* html */ `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>${title}</title>
+    <style>
+      #message {
+        white-space: pre-wrap;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="message"></div>
+  </body>
+</html>
+`,
+  })
+
+  setupTemplate({
+    filename: 'main.ts',
+    content: `
+import { app, BrowserWindow } from 'electron'
+import { resolve } from 'path'
+
+function createWindow() {
+  let win = new BrowserWindow({
+    webPreferences: {
+      preload: resolve('dist/preload.js'),
+    },
+  })
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length == 0) {
+    createWindow()
+  }
+})
+`,
+  })
+
+  setupTemplate({
+    filename: 'preload.ts',
+    content: `
+declare let message: HTMLDivElement
+
+window.addEventListener('DOMContentLoaded', () => {
+  message.textContent =
+    new Date() + '\\n' + JSON.stringify(process.versions, null, 2)
+})
+`,
+  })
+
+  setupTemplate({
+    filename: 'tsconfig.json',
+    content: {
+      compilerOptions: {
+        target: 'es2022',
+        module: 'commonjs',
+        esModuleInterop: true,
+        forceConsistentCasingInFileNames: true,
+        strict: true,
+        skipLibCheck: true,
+        incremental: true,
+        outDir: 'dist',
+      },
+      exclude: ['dist'],
+    },
+  })
+
+  setupTemplate({
+    filename: 'package.json',
+    content: {
+      scripts: {
+        'start': 'run-p main tsc:watch',
+        'main': 'electron -r ts-node/register main.ts',
+        'tsc:watch': 'tsc -p . --watch',
+        'test': 'tsc --noEmit',
+        'build': 'tsc -p .',
+      },
+      devDependencies: {
+        '@types/node': '^20.10.0',
+        'electron': '^26.6.1',
+        'npm-run-all': '^4.1.5',
+        'ts-node': '^10.9.1',
+        'ts-node-dev': '^2.0.0',
+        'typescript': '^5.3.2',
+      },
+    },
+  })
+
+  setupTemplate({
+    filename: '.gitignore',
+    content: `
+res/
+node_modules/
+dist/
+*-lock.json
+*-lock.yaml
+*.lock
+.*-debug.log
+*.tsbuildinfo
+.env
+*.tgz
+.DS_Store
+.parcel-cache
+`,
+  })
+
   console.log(
     `
 done.
